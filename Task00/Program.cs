383af029a237ece6f36a690baa17bcb5e2fe3a96
XBearH <1480049710@qq.com>
2026-09-24 18:58:05 +0800

    Complete Task00 Hello and Input

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -2,20 +2,22 @@
 
 //Task00: 'Hello' and Input
 
+/*
 Console.WriteLine("Welcome to study C#!");
-// Console.WriteLine("May I know your name?");
-// string name = Console.ReadLine();
-// Console.WriteLine($"Hello, {name}!");
-// Console.WriteLine($"OK, {name}, how old are you?");
-// string age = Console.ReadLine();
-// // if(age == "1")
-// // {
-// //     Console.WriteLine($"{name}, you are one year old!");
-// // }
-// // else
-// // {
-// //     Console.WriteLine($"You are {age} years old, {name}!");
-// // }
+Console.WriteLine("May I know your name?");
+string name = Console.ReadLine();
+Console.WriteLine($"Hello, {name}!");
+Console.WriteLine($"OK, {name}, how old are you?");
+string age = Console.ReadLine();
+if(age == "1")
+{
+    Console.WriteLine($"{name}, you are one year old!");
+}
+else
+{
+    Console.WriteLine($"You are {age} years old, {name}!");
+}
+*/
 Console.WriteLine("Who are you and how old are you?");
 string name = Console.ReadLine();
 string age = Console.ReadLine();
